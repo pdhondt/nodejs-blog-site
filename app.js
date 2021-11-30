0450be411a9402d2282b42a83f0887a7d26d1f99
Peter D'hondt <dhondt.peter@gmail.com>
2021-11-30 14:05:09 +0100

    added route handlers and views folder

--- app.js
+++ app.js
@@ -8,5 +8,17 @@ app.listen(port, (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Initial project setup");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
